46cc13446019b4951fe089d5a58a603bcdd07b50
Woocash7 <38039234+Woocash7@users.noreply.github.com>
2019-10-31 17:08:13 +0100

    Add files via upload

---
+++ website/music/urls.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from django.urls import path, re_path
+from . import views 
+
+app_name = 'music'
+
+urlpatterns = [
+    # /music/
+    path('', views.IndexView.as_view(), name='index'),
+
+    path('register/', views.UserFormView.as_view(), name='register'),
+    
+    # re_path(r'^(?P<pk>[0-9]+)/$')
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+
+    # /music/album/add/
+    path('album/add/', views.AlbumCreate.as_view(), name='album-add'),
+    
+    # /music/album/2/
+    path('album/<int:pk>/', views.AlbumUpdate.as_view(), name='album-update'),
+    
+    # /music/album/2/delete
+    path('album/<int:pk>/delete/', views.AlbumDelete.as_view(), name='album-delete'),
+] 
